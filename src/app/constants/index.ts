0166be6fddae8e8f070299f9cc845352dd18b5d6
unknown <pafrasson@gmail.com>
2024-02-29 00:44:34 -0300

    first commit

---
+++ src/app/constants/index.ts
@@ -0,0 +1,47 @@
+export const inclusions = [
+    {
+      title: 'Frete Grátis',
+      description: 'Frete Grátis para pedidos acima de R$250',
+      icon: '/assets/icons/shipping.svg',
+    },
+    {
+      title: 'Garantia de trocas',
+      description: '30 dias para trocar o produto',
+      icon: '/assets/icons/dollar.svg',
+    },
+    {
+      title: 'Suporte On-line',
+      description: 'Segunda a Sexta (9:00 - 11:00) (12:30 - 18:00) Sábado (9:00 - 13:00)',
+      icon: '/assets/icons/support.svg',
+    },
+    {
+      title: 'Pagamento Flexível',
+      description: 'Pague com múltiplas formas de pagamento',
+      icon: '/assets/icons/payment.svg',
+    },
+  ]
+  
+  export const profileNavItems = [
+    {
+      title: 'Informações Pessoais',
+      url: '/account',
+      icon: '/assets/icons/user.svg',
+    },
+    {
+      title: 'Minhas Compras',
+      url: '/account/purchases',
+      icon: '/assets/icons/purchases.svg',
+    },
+    {
+      title: 'Meus Pedidos',
+      url: '/account/orders',
+      icon: '/assets/icons/orders.svg',
+    },
+    {
+      title: 'Logout',
+      url: '/logout',
+      icon: '/assets/icons/logout.svg',
+    },
+  ]
+  
+  export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']  
